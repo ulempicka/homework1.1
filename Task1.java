219abb43f85e60e481c9bc186eab1903f0088c1a
ulempicka <urszula.lempicka@gmail.com>
2020-06-03 22:29:09 +0200

    modified Task

--- Task1.java
+++ Task1.java
@@ -3,5 +3,7 @@ public class Task1 {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+		System.out.println("i");
+		System.out.println("commitowania");
     }
 }
